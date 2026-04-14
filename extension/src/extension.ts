1037575cac1a96b9d03e9ee9cfa42d01c6ba7584
Jack Fraser-Govil <j.fraser-govil@bham.ac.uk>
2026-04-14 14:21:30 +0100

    Reboot, Boot, Pause and Resume commands now implemented within extension

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -6,16 +6,26 @@ export function activate(context: vscode.ExtensionContext): void
 	const launchCommand = vscode.commands.registerCommand('cortex.launch', () => {
         vscode.window.showInformationMessage('Cortex is launching...');
     });
-	const rebootCommand = vscode.commands.registerCommand('cortex.reboot', () => {
-        vscode.window.showInformationMessage('Cortex is launching...');
-    });
-
-
+	vscode.window.showInformationMessage('Cortex is launching...');
+	
 	//metadata & link engine
 	const engine = new CortexEngine();
 	context.subscriptions.push(engine);
-
-
+	
+	
+	const rebootCommand = vscode.commands.registerCommand('cortex.reboot', () => {
+		vscode.window.showInformationMessage('Cortex is relaunching...');
+		engine.bootCortex();
+    });
+	const pauseCommand = vscode.commands.registerCommand('cortex.pause', () => {
+		vscode.window.showInformationMessage('Cortex Compilation Is Paused');
+		engine.EngineCommand("pause");
+    });
+	const resumeCommand = vscode.commands.registerCommand('cortex.resume', () => {
+		engine.EngineCommand("resume");
+		engine.bootCortex();
+    });
+	
 }
 
 export function deactivate()
